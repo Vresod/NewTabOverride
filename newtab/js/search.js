20639da7932dfd6bf1042db8bff743c8d48ed775
Vresod <40863375+Vresod@users.noreply.github.com>
2018-10-01 18:34:11 -0400

    vc

---
+++ newtab/js/search.js
@@ -0,0 +1,11 @@
+function search() {
+	var goto = 'https:&#47;&#47;' + prompt('Where are you going?')
+	var confirmed = confirm('Are you sure?')
+	if (confirmed = true) {
+		var location = goto;
+	}
+}
+
+function test() {
+	document.write('test')
+}
